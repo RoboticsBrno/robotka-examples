ca97797ee176eb0cc2d7cd0df24040eed6776ebc
Gitea <gitea@fake.local>
2020-07-09 20:30:58 +0200

    Tested ultrasound example.

--- ultrasound/src/main.cpp
+++ ultrasound/src/main.cpp
@@ -1,69 +1,72 @@
 #include "robotka.h"
 
+// Rozsviti 0-4 LEDky podle toho, jakou vzdalenost nameri ultrazvukovy senzor.
+// Kazda rozsvicena LEDka odpovida 10 cm namerene vzdalenosti.
 void setLedsByDistance(uint32_t distance) {
     // rozdeleno na useky po 100 mm (10 cm)
     if (distance < 100) {
         rkLedAll(false);
+        printf("0\n");
     } else if (distance < 200) {
         rkLedById(1, true);
         rkLedById(2, false);
         rkLedById(3, false);
         rkLedById(4, false);
+        printf("1\n");
     } else if (distance < 300) {
         rkLedById(1, true);
         rkLedById(2, true);
         rkLedById(3, false);
         rkLedById(4, false);
+        printf("2\n");
     } else if (distance < 400) {
         rkLedById(1, true);
         rkLedById(2, true);
         rkLedById(3, true);
         rkLedById(4, false);
+        printf("3\n");
     } else {
         rkLedAll(true);
+        printf("4\n");
     }
 }
 
-void updateBeeping(uint32_t beepingPeriodMs) {
+// Pipani pomoci bzucaku podobne jako u senzoru v aute.
+void beepLikeCarSensor(uint32_t beepingPeriodMs) {
     static bool isActive = false;
     static uint32_t nextChangeMs = 0;
 
     if (millis() < nextChangeMs) {
         return;
     } else {
-        isActive = !isActive || (beepingPeriodMs < 100);
+        isActive = (!isActive || (beepingPeriodMs < 100)) && (beepingPeriodMs < 1000);
         rkBuzzerSet(isActive);
-        nextChangeMs += beepingPeriodMs;
+        nextChangeMs += beepingPeriodMs/4;
     }
 }
 
+// Piskani bzucakem, pokud je namerena vzdalenost mensi nez 300 mm.
 void beepByDistance(uint32_t distance) {
     if (distance < 300)
         rkBuzzerSet(true);
     else
         rkBuzzerSet(false);
-    //updateBeeping( (distance - distance % 100) );
 }
 
 // Funkce setup se zavolá vždy po startu robota.
 void setup() {
-    printf("0 \n");
     rkConfig cfg;
     // Upravte nastavení, například:
     // cfg.motor_max_power_pct = 30; // limit výkonu motorů na 30%
     rkSetup(cfg);
 
-    printf("1\n");
-    while (true) {
-        //uint32_t distance = 10000; // max 10 m
-        //for(int id=1; id<=4; id++) {
-        //    distance = max(distance, rkUltraMeasure(id));
-        //}
-        printf("2 looooooooooong string\n");
+    while (!rkButtonIsPressed(BTN_DOWN)) {
         uint32_t distance = rkUltraMeasure(1);
         printf("%d\n", distance);
 
-        //setLedsByDistance(distance);
+        setLedsByDistance(distance);
         //beepByDistance(distance);
+        beepLikeCarSensor(distance);
     }
+    while(true);
 }
