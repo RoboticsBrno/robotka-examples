04b8697d6b44be054c957f6cfa280fabed785481
haberturdeur <tomas.rohlinek@outlook.cz>
2020-07-12 11:13:51 +0200

    Change smart_leds example to better fit expectations
    Update library version
    Add explanatory comments
    Switch from static to global variable

--- smart-leds/src/main.cpp
+++ smart-leds/src/main.cpp
@@ -1,24 +1,29 @@
 #include "robotka.h"
 
+//speciální typ konsatanty, nejdou do ní ukládat hodnoty. Tato popisuje počet LED na pásku.
 #define LED_COUNT 8
 
+//Globálně definovaná proměnná, je dostupná z celého programu
+uint8_t hue = 0;
+
 void showGradient() {
-    static uint8_t hue;
-    hue++;
+    hue += 15;
     for (int i = 0; i != LED_COUNT; i++) {
+        //model HSV: první proměnná mění barvu, druhá intenzitu barev a třetí intenzitu svícení.¨
+        // Oproti RGB stačí pro změnu barvy měnit pouze jednu hodnotu.
         rkSmartLedsHSV(i, (hue + 30 * i), 255, 255);
     }
 }
 
 void showRGB(uint8_t r, uint8_t g, uint8_t b) {
     for (int i = 0; i < LED_COUNT; i++) {
+        //model RGB: první proměnná = intenzita červené, druhá proměnná = intenzita zelené, třetí proměnná = intenzita modré
         rkSmartLedsRGB(i, r, g, b);
     }
 }
 
 void setup() {
     rkConfig cfg;
-    cfg.smart_leds_count = LED_COUNT; //výchozí hodnota je 8
     rkSetup(cfg);
 
     while (true) {
