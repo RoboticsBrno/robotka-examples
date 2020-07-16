a4f5e06edbfaac2111bf59e92b43f4f76f32353f
Vojtěch Boček <vojtech.bocek@avast.com>
2020-07-16 13:35:48 +0200

    Update library to v1.3.3

--- .update_roboruka_lib.sh
+++ .update_roboruka_lib.sh
@@ -9,3 +9,6 @@ for f in $(find . -maxdepth 2 -type f -name platformio.ini); do
     echo "$f"
     sed -i "s=\(RB3204-RBCX-Robotka-library/archive/v\)[0-9.]\+\.zip=\1$1.zip=" "$f"
 done
+
+git commit -a -m "Update library to v$1"
+git show
